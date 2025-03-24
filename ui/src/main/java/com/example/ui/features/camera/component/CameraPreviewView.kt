5adcb9b6c26913eb999e94e5596ba286513e2f1c
AnthonyMde <anthony.mamode@gmail.com>
2025-03-24 15:27:32 +0100

    [feat] Add camera preview.

--- ui/src/main/java/com/example/ui/features/camera/component/CameraPreviewView.kt
+++ ui/src/main/java/com/example/ui/features/camera/component/CameraPreviewView.kt
@@ -1,16 +1,38 @@
 package com.example.ui.features.camera.component
 
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
 fun CameraPreviewView(
     modifier: Modifier = Modifier
 ) {
-    Box(modifier = modifier, contentAlignment = Alignment.Center) {
-        Text("Permissions allowed \uD83D\uDD25")
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val cameraController = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(CameraController.IMAGE_CAPTURE)
+        }
+    }
+
+    Box(modifier = modifier) {
+        AndroidView(
+            factory = { context ->
+                PreviewView(context).apply {
+                    controller = cameraController
+                    cameraController.bindToLifecycle(lifecycleOwner)
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
--- ui/src/main/java/com/example/ui/features/home/HomeScreen.kt
+++ ui/src/main/java/com/example/ui/features/home/HomeScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.ui.features.home.component.HomeHeaderView
 import com.example.ui.features.home.component.PublicationItemView
