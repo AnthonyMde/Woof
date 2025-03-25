dfca2700a4bf8949653bc4612de25a9d394d3dc6
AnthonyMde <anthony.mamode@gmail.com>
2025-03-25 19:29:40 +0100

    [ui] Add splashscreen.

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.woof.app.MainScreen
 import com.example.ui.theme.Dimensions
 import com.example.ui.theme.LocalDimensions
@@ -12,6 +13,7 @@ import com.example.ui.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
